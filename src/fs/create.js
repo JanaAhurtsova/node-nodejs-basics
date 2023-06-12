aaf2e3678e289a082e1ae5bd38f17ae8e73a44f6
Jana Ahurtsova <byjankaaa@gmail.com>
2023-06-12 12:14:37 +0300

    feat: implement function that creates new file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import { writeFile } from 'fs/promises';
+import path from 'path';
+
+const content = 'I am fresh and young';
+const dirname = 'src/fs/files';
+const file = path.resolve(dirname, 'fresh.txt');
+const error = 'FS operation failed';
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await writeFile(file, content, { flag: 'wx' });
+  } catch(err) {
+    console.log(err);
+    throw new Error(error);
+  }
 };
 
 await create();
