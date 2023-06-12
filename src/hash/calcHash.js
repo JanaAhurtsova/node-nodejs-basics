1f76e52f9979b33d6be10b0e82e27f040f382372
Jana Ahurtsova <byjankaaa@gmail.com>
2023-06-13 02:21:15 +0300

    feat: add function that calculates SHA256 hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import { readFile } from 'fs/promises';
+import { createHash } from 'crypto';
+import path from 'path';
+
+const FILE_PATH = 'src/hash/files/fileToCalculateHashFor.txt';
+const dirname = path.resolve(FILE_PATH);
+
 const calculateHash = async () => {
-    // Write your code here 
+  try {
+    const content = await readFile(dirname);
+    const hash = createHash('SHA256').update(content).digest('hex');
+    console.log(hash);
+  } catch(err) {
+    console.log(err);
+  }
 };
 
 await calculateHash();
