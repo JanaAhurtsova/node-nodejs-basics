7895f64099d19856b23741b20a7bd27d2d226df8
Jana Ahurtsova <byjankaaa@gmail.com>
2023-06-12 19:18:08 +0300

    feat: add function that prints array of filenames

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,14 @@
+import { readdir } from 'fs/promises';
+import { ERROR, FILES_FOLDER_PATH } from "../constants/Constants.js";
+
 const list = async () => {
-    // Write your code here 
+  try {
+    const files = await readdir(FILES_FOLDER_PATH);
+    console.log(files);
+  } catch(err) {
+    console.log(err);
+    throw new Error(ERROR);
+  }
 };
 
 await list();
