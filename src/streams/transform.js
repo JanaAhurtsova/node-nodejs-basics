e2fca72a2246683c8f0d55fe5d7fc1f462ea1d6e
Jana Ahurtsova <byjankaaa@gmail.com>
2023-06-13 22:10:59 +0300

    feat: finish stream transform task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,15 @@
+import { Transform } from 'stream';
+import { pipeline } from 'stream/promises';
+
 const transform = async () => {
-    // Write your code here 
+  const transformStream = new Transform({
+    transform(data, _, callback) {
+      const reverseData = data.toString().split('').reverse().join('');
+      callback(null, reverseData);
+    },
+  });
+
+  await pipeline(process.stdin, transformStream, process.stdout);
 };
 
 await transform();
