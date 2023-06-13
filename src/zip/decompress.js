491932722806e6efb1c9c54baf32c08fb16734c8
Jana Ahurtsova <byjankaaa@gmail.com>
2023-06-13 23:18:59 +0300

    feat: implement zlib

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import { createUnzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { pipeline } from 'stream/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const fileDecompress = path.resolve(__dirname, './files/archive.gz');
+const decompressedFile = path.resolve(__dirname, './files/fileToCompress.txt');
+
 const decompress = async () => {
-    // Write your code here 
+  const inp = createReadStream(fileDecompress);
+  const out = createWriteStream(decompressedFile);
+  await pipeline(inp, createUnzip(), out);
 };
 
 await decompress();
