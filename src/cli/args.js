f6523e56d54421901f6992865a96ac5ef8c31a8c
Jana Ahurtsova <byjankaaa@gmail.com>
2023-06-15 22:43:03 +0300

    feat: add function with child process

--- src/cli/args.js
+++ src/cli/args.js
@@ -2,7 +2,7 @@ import { argv } from 'node:process';
 import { PREFIX_ARGS } from '../constants/Constants.js';
 
 const parseArgs = () => {
-  const formattedArr = argv.reduce((acc, value, index, arr) => {
+  const formattedArr = argv.slice(2).reduce((acc, value, index, arr) => {
     if(value.startsWith(PREFIX_ARGS)) {
       const formattedArgs = `${value.replace(PREFIX_ARGS, '')} is ${arr[index + 1]}`;
       return [...acc, formattedArgs];
